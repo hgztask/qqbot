4547dfc7b77b52e5812bb45320485d8f0725b003
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-12 14:21:13 +0800

    完善功能,支持修改本地文件

--- src/main/java/com/example/qqbot/model/ReReadingModel.java
+++ src/main/java/com/example/qqbot/model/ReReadingModel.java
@@ -1,10 +1,11 @@
 package com.example.qqbot.model;
 
-import cn.hutool.json.JSONArray;
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.example.qqbot.SignalUtil;
 import com.example.qqbot.data.DataGroup;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
@@ -31,11 +32,12 @@ public class ReReadingModel implements Runnable {
     /**
      * 指定存储复读机成员的路径
      */
-    private static final File pathFile = new File("E:\\复读成员.json");
+    private static final File PATH_FILE = new File("E:\\复读成员.json");
     /**
      * 指定要复读机的成员
      */
-    private static final List<String> RE_READING_MEMBER_SET = getFileJson(pathFile);
+    @Getter
+    private static final List<String> RE_READING_MEMBER_SET = getFileJson(PATH_FILE);
 
     public ReReadingModel(DataGroup dataGroup) {
         this.dataGroup = dataGroup;
@@ -54,20 +56,74 @@ public class ReReadingModel implements Runnable {
         return new ArrayList<>(jsonArray);
     }
 
+    /**
+     * 添加要复读的人员
+     */
+    public static void addReReadingMemberSet(DataGroup dataGroup, String userATID) {
+        if (RE_READING_MEMBER_SET.contains(userATID)) {
+            JSONObject json = SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "该用户已经设置过了!");
+            if (json.isEmpty()) {
+                log.info("设置要复读的人员功能模块-回复设置过-请求失败了");
+            }
+            return;
+        }
+        RE_READING_MEMBER_SET.add(userATID);
+        //修改本地文件
+        FileUtil.writeUtf8String(RE_READING_MEMBER_SET.toString(), PATH_FILE);
+        JSONObject json = SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "已将该成员设置为复读机对象");
+        if (json.isEmpty()) {
+            log.info("设置要复读的人员功能模块-已将该成员设置对象-请求失败了");
+            return;
+        }
+        log.info("设置要复读的人员功能模块-已将该成员设置对象-请求成功!");
+    }
+
+    /**
+     * 指定删除的复读机成员对象
+     *
+     * @param dataGroup
+     */
+    public static void removeReReadingMemberSet(DataGroup dataGroup, String userATID) {
+        if (RE_READING_MEMBER_SET.remove(userATID)) {
+            //修改本地文件
+            FileUtil.writeUtf8String(RE_READING_MEMBER_SET.toString(), PATH_FILE);
+            if (SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "已将该对象移除至集合对象!").isEmpty()) {
+                log.info("指定删除的复读机成员对象-已将该对象移除至集合对象-请求成功!");
+                return;
+            }
+            log.info("指定删除的复读机成员对象-请求成功!");
+            return;
+        }
+        if (SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "移除失败,没有该复读机成员对象!").isEmpty()) {
+            log.info("指定删除的复读机成员对象-移除失败-请求成功!");
+            return;
+        }
+        log.info("指定删除的复读机成员对象-移除失败-请求成功!");
+    }
+
+    /**
+     * 打印复读机集合里的对象
+     */
+    public static void printReReadingMemberSet(DataGroup dataGroup) {
+        if (SignalUtil.sendPrivateMessage(dataGroup.getUser_id(), String.format("复读机对象成员:\n%s", JSONUtil.parseArray(RE_READING_MEMBER_SET).toStringPretty())).isEmpty()) {
+            SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "打印复读机集合里的对象-响应失败!");
+            return;
+        }
+        SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "信息已私发到超级用户上!");
+        log.info("打印复读机集合里的对象模块功能消息发送成功!");
+    }
+
 
     @Override
     public void run() {
         String raw_message = dataGroup.getRaw_message();
         String user_id = dataGroup.getUser_id();
-        if (raw_message.contains("设置要复读机的成员=")) {
-            //设置后面台添加设置对象
-        } else if (RE_READING_MEMBER_SET.isEmpty() || !(RE_READING_MEMBER_SET.contains(user_id))) {
-            return;
-        }
         JSONObject json = SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), raw_message);
         if (json.isEmpty()) {
             return;
         }
         log.info("复读机请求成功!");
     }
+
+
 }
