c16c106011212eb4bef0b26815c3946db2966622
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-13 00:03:37 +0800

    新增对监听群聊集合与推送群聊集合对象的增删打印操作

---
+++ src/main/java/com/example/qqbot/model/group/ListeningGroupModel.java
@@ -0,0 +1,168 @@
+package com.example.qqbot.model.group;
+
+import cn.hutool.json.JSONUtil;
+import com.example.qqbot.Util.SignalUtil;
+import com.example.qqbot.data.DataUserEights;
+import com.example.qqbot.data.group.DataGroup;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * 监听群消息
+ * <p>
+ * 功能
+ * 监听指定群聊的消息,当有更新时.将对应的消息原封不动通过机器人推送到指定的推送全
+ *
+ * @author byhgz
+ * @version 1.0
+ * @date 2023/2/12 22:25
+ */
+@Slf4j
+public class ListeningGroupModel implements Runnable {
+
+    /**
+     * 单例对象
+     */
+    @Getter
+    private static final ListeningGroupModel listeningGroupModel = new ListeningGroupModel();
+
+
+    /**
+     * 监听的群
+     */
+    private static final Set<String> LISTENINGGROUPIDSET = new LinkedHashSet<>();
+
+
+    /**
+     * 推送消息的群聊
+     */
+    private static final Set<String> PUSHGROUPIDSET = new LinkedHashSet<>();
+
+
+    static {
+        LISTENINGGROUPIDSET.add("935671622");
+        LISTENINGGROUPIDSET.add("942611877");
+        PUSHGROUPIDSET.add("760849278");
+    }
+
+    @Setter
+    private DataGroup dataGroup;
+
+    @Override
+    public void run() {
+        if (dataGroup == null) {
+            throw new NullPointerException("dataGroup为null");
+        }
+        if (!(LISTENINGGROUPIDSET.contains(dataGroup.getGroup_id()))) {
+            return;
+        }
+        if ("202619505".equals(dataGroup.getUser_id())) {
+            //如果监听的群聊里触发该功能的包含了该成员.则不推送
+            return;
+        }
+
+        for (String pushGroupID : PUSHGROUPIDSET) {
+            if (SignalUtil.sendGroupMessage(pushGroupID, dataGroup.getRaw_message()).isEmpty()) {
+                log.info("监听群聊推送消息失败!");
+                return;
+            }
+            log.info("监听群聊推送消息成功!");
+
+        }
+
+    }
+
+
+    /**
+     * 打印接受推送消息的群聊集合
+     * 推荐私聊窗口
+     *
+     * @param user_id 用户
+     */
+    public static void printPushGather(String user_id) {
+        if (SignalUtil.sendPrivateMessage(user_id, JSONUtil.parseArray(PUSHGROUPIDSET).toStringPretty()).isEmpty()) {
+            log.info("打印接受推送消息的群聊集合-失败!");
+            return;
+        }
+        log.info("打印接受推送消息的群聊集合-成功!");
+    }
+
+
+    /**
+     * 打印监听群聊的集合
+     *
+     * @param user_id
+     */
+    public static void printlisteninggroupGather(String user_id) {
+        if (SignalUtil.sendPrivateMessage(user_id, JSONUtil.parseArray(LISTENINGGROUPIDSET).toStringPretty()).isEmpty()) {
+            log.info("打印监听群聊的集合-失败!");
+            return;
+        }
+        log.info("打印监听群聊的集合-成功!");
+    }
+
+
+    /**
+     * 往监听群聊的对象
+     * 请确保机器人所在该群
+     *
+     * @param group_id 群号,请确保机器人所在该群
+     */
+    public static void addlisteninggroupGather(String group_id) {
+        if (LISTENINGGROUPIDSET.add(group_id)) {
+            log.info("往监听群聊的对象-失败!,该群已经在监听集合里了");
+            SignalUtil.sendGroupMessage(group_id, "添加失败!,该群已经在监听集合里了!");
+            return;
+        }
+        SignalUtil.sendGroupMessage(group_id, "已添加标记当前群为监听对象!");
+    }
+
+    /**
+     * 删除监听群聊集合里指定的群聊对象
+     *
+     * @param group_id
+     */
+    public static void removelisteninggroupGather(String group_id) {
+        if (LISTENINGGROUPIDSET.remove(group_id)) {
+            log.info("删除监听群聊集合里指定的群聊对象-失败!,该群并不在监听群聊集合里");
+            SignalUtil.sendGroupMessage(group_id, "删除失败,该群并不在监听群聊集合里");
+            return;
+        }
+        SignalUtil.sendGroupMessage(group_id, "已移除取消标记当前群聊的监听状态!");
+    }
+
+
+    /**
+     * 添加标记指定群聊作为推送群聊
+     * 添加到推送群聊集合对象
+     * @param group_id 群号
+     */
+    public static void addPoshGroupGather(String group_id) {
+        if (PUSHGROUPIDSET.add(group_id)) {
+            log.info("添加标记指定群聊作为推送群聊-添加失败!");
+            SignalUtil.sendGroupMessage(group_id, "标记当前群为推送群聊失败,已经添加过了!");
+            return;
+        }
+        log.info("添加标记指定群聊作为推送群聊-添加成功!");
+        SignalUtil.sendGroupMessage(group_id, "已标记当前群为推送群聊");
+    }
+
+    /**
+     * 取消标记指定群聊作为推送群聊
+     * 从推送集合对象里移除
+     * @param group_id
+     */
+    public static void removePoshGroupGather(String group_id) {
+        if (PUSHGROUPIDSET.remove(group_id)) {
+            log.info("取消标记指定群聊作为推送群聊-添加失败!");
+            SignalUtil.sendGroupMessage(group_id, "取消标记当前群为推送群聊失败,当前群并不在推送集合内");
+            return;
+        }
+    }
+
+
+}
