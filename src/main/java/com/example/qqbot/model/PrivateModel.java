dc0f3495da1854f23802692f8252ed3d41de282b
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-13 11:44:09 +0800

    更改if语句层级,新增接受打印推送消息的群聊集合

--- src/main/java/com/example/qqbot/model/PrivateModel.java
+++ src/main/java/com/example/qqbot/model/PrivateModel.java
@@ -1,10 +1,11 @@
 package com.example.qqbot.model;
 
 import cn.hutool.json.JSONObject;
-import com.example.qqbot.SignalUtil;
+import com.example.qqbot.Util.SignalUtil;
 import com.example.qqbot.data.DataPrivate;
 import com.example.qqbot.data.DataUserEights;
-import org.junit.jupiter.api.Test;
+import com.example.qqbot.model.group.GroupModel;
+import com.example.qqbot.model.group.ListeningGroupModel;
 
 import java.util.HashMap;
 
@@ -28,20 +29,29 @@ public class PrivateModel implements Runnable {
         String message = dataPrivate.getMessage();
         //原始消息的内容
         String raw_message = dataPrivate.getRaw_message();
+        //发言者QQ号
         String user_id = dataPrivate.getUser_id();
-        if (raw_message.startsWith("刷新复读机成员") && DataUserEights.SUPERUSER.contains(user_id)) {
+        boolean boolSupeRuser = DataUserEights.SUPERUSER.contains(user_id);
+        if (raw_message.startsWith("刷新复读机成员") && boolSupeRuser) {
             //需要超级用户权限
             //该关键词触发条件要优先于下面的复读机,要不然会导致复读操作
-            ReReadingModel.readFileArraySetRE_READING_MEMBER_SET(user_id);
+            ReReadingModel.readfilearraysetre_reading_member_set(user_id);
             return;
-        } else if (raw_message.startsWith("打印触发复读机关键词") && DataUserEights.SUPERUSER.contains(user_id)) { //需要超级用户权限
+        }
+        if (raw_message.startsWith("打印触发复读机关键词") && boolSupeRuser) { //需要超级用户权限
             ReReadingModel.printKeySet(user_id);
             return;
-        } else if (raw_message.startsWith("打印复读机成员") && DataUserEights.SUPERUSER.contains(user_id)) {//需要超级用户权限
+        }
+        if (raw_message.startsWith("打印复读机成员") && boolSupeRuser) {//需要超级用户权限
             ReReadingModel.printReReadingMemberSet(user_id);
             return;
-        } else if (raw_message.startsWith("刷新黑名单群聊数据") || DataUserEights.SUPERUSER.contains(user_id)) {//需要超级用户权限
+        }
+        if (raw_message.startsWith("刷新黑名单群聊数据") && boolSupeRuser) {//需要超级用户权限
             GroupModel.readFIlePathBlackList(user_id);
+            return;
+        }
+        if (raw_message.startsWith("打印接受推送消息的群聊集合") && boolSupeRuser) {
+            ListeningGroupModel.printPushGather(user_id);
             return;
         }
         //实现一个复读机
