2c36a0a7c8cc9ccfaef9b864e8f1cba61d2152e5
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-16 18:47:00 +0800

    完善该模块

--- src/main/java/com/example/qqbot/model/LanZouYmodel/LanZuoCloudResourceSearch.java
+++ src/main/java/com/example/qqbot/model/LanZouYmodel/LanZuoCloudResourceSearch.java
@@ -10,13 +10,17 @@ import com.example.qqbot.Util.InformationUtil;
 import com.example.qqbot.Util.SignalUtil;
 import com.example.qqbot.data.Message;
 import com.example.qqbot.data.group.DataGroup;
+import com.example.qqbot.data.json.DataJson;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -40,53 +44,78 @@ public class LanZuoCloudResourceSearch implements Runnable, IMessageEvent {
 
     @Override
     public void run() {
+        String user_id = dataGroup.getUser_id();
+        String group_id = dataGroup.getGroup_id();
         String key = InformationUtil.subEqual("=", dataGroup.getRaw_message());
         StringBuilder messageBuilder = new StringBuilder();
-        int index = 0, eligibleIndex = 0;
-        HashMap<String, String> sendMap = new HashMap<>();
-        sendMap.put("group_id", dataGroup.getGroup_id());
-        sendMap.put("message", String.valueOf(messageBuilder));
-        SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "正在搜索中,请稍等...");
+        int tempIndex = 0;
+        int index = 0;
+        SignalUtil.sendGroupMessage(group_id, "正在搜索中,请稍等...");
+        Set<JSONObject> dataArraySet = new LinkedHashSet<>();
         for (int i = 0; i < readJSONArray.size(); i++) {
             String name = readJSONArray.getByPath(String.format("[%s]name", i), String.class);
             if (!(name.contains(key))) {
                 continue;
             }
             //循环状态时符合条件的数量达到5个时执行分批发送并清空记录值和StringBuilder
-            if (index == 35) {
-                startGet(messageBuilder);
-                index = 0;
+            if (tempIndex == 5) {
+                dataArraySet.add(DataJson.text(messageBuilder));
+                tempIndex = 0;
                 //达到5条数目时重新建立一个StringBuilder对象
                 messageBuilder = new StringBuilder();
             }
             //找到符合条件的资源就加1
+            tempIndex++;
             index++;
-            //记录符合条件的总个数
-            eligibleIndex++;
             String format = CharSequenceUtil.format("软件名={}下载地址={}\n", name, readJSONArray.getByPath(String.format("[%s]url", i), String.class));
             messageBuilder.append(format);
         }
         if (messageBuilder.isEmpty()) {
-            SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "未匹配到内容,本轮故结束!");
-        } else {
-            //当messageBuilder对象还有内容时候发送消息,说明符合关键词的资源小于5个的
-            messageBuilder.append(String.format("===========搜索完成!符合关键词资源个数%s个===========", eligibleIndex));
-            startGet(messageBuilder);
-        }
-    }
-
-    @SneakyThrows
-    private void startGet(StringBuilder messageBuilder) {
-        //休息一秒之后继续发送
-        TimeUnit.SECONDS.sleep(1);
-        JSONObject jsonObject = SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), String.valueOf(messageBuilder));
-
-        if (jsonObject.isEmpty()) {
-            log.info("发送失败");
+            SignalUtil.sendGroupMessage(group_id, "未匹配到内容,本轮故结束!");
             return;
         }
-        log.info("发送成功!");
+
+        dataArraySet.add(DataJson.text(messageBuilder));  //当上面for语句中的未达到限定数时,说明剩下的没有添加
+
+        //dataArraySet.size()/200
+
+
+        //这里功能先将就用,后面有需要时再写
+        int size = dataArraySet.size();
+        //至少要发几次
+        int exceptIndex = size / 200;
+        //多出来的消息
+        int exceptForIndex = size % 200;
+        if (exceptIndex == 0) {
+            //说明只需要发1次合并聊天记录即可
+            //return;
+        }
+        //能被整除,对应啊哦执行几次exceptIndex
+        //不能被整除就在exceptIndex+1次即可
+
+
+        if (size <= 200) {
+            JSONArray jsonArray = DataJson.nodeText("机器人", user_id, dataArraySet);
+            //目前已知合并转发不支持内容中的消息太大,但消息条数,测试至少可以达到78条,意思就是尽量不要单条消息字符串太多,
+            SignalUtil.sendGroupForwardMsg(group_id, jsonArray);
+            SignalUtil.sendGroupMessage(group_id, String.format("===========搜索完成!符合关键词资源个数%s个===========", dataArraySet.size()));
+            return;
+        }
+        Set<JSONObject> tempSet = new LinkedHashSet<>();
+        for (JSONObject jsonObject : dataArraySet) {
+            tempSet.add(jsonObject);
+            if (tempSet.size() == 200) {
+                break;
+            }
+        }
+        JSONArray jsonArray = DataJson.nodeText("机器人", user_id, tempSet);
+        //目前已知合并转发不支持内容中的消息太大,但消息条数,测试至少可以达到200条,意思就是尽量不要单条消息字符串太多,
+        SignalUtil.sendGroupForwardMsg(group_id, jsonArray);
+        SignalUtil.sendGroupMessage(group_id, String.format("搜索完成!符合关键词资源个数%s个,数量太大故截取,剩下%s", dataArraySet.size(), tempSet.size()));
+
+
     }
+
 
     /**
      * 权重,权重高的值会先匹配
@@ -95,8 +124,9 @@ public class LanZuoCloudResourceSearch implements Runnable, IMessageEvent {
      */
     @Override
     public int weight() {
-        return 0;
+        return 6;
     }
+
 
     /**
      * 接受消息
