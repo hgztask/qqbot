5cecadd01853b5e57d9e4498f3ea48c06461b70c
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-16 16:01:40 +0800

    优化蓝奏云搜索

--- src/main/java/com/example/qqbot/model/LanZouYmodel/LanZuoCloudResourceSearch.java
+++ src/main/java/com/example/qqbot/model/LanZouYmodel/LanZuoCloudResourceSearch.java
@@ -1,13 +1,18 @@
 package com.example.qqbot.model.LanZouYmodel;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import com.example.qqbot.Event.IMessageEvent;
+import com.example.qqbot.Util.InformationUtil;
 import com.example.qqbot.Util.SignalUtil;
+import com.example.qqbot.data.Message;
 import com.example.qqbot.data.group.DataGroup;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
@@ -22,13 +27,10 @@ import java.util.concurrent.TimeUnit;
  * @date 2023/2/11 12:14
  */
 @Slf4j
-public class LanZuoCloudResourceSearch implements Runnable {
+@Component
+public class LanZuoCloudResourceSearch implements Runnable, IMessageEvent {
 
     private static final JSONArray readJSONArray = JSONUtil.readJSONArray(new File("E:\\72030条蓝奏云资源.json"), StandardCharsets.UTF_8);
-    /**
-     * 搜索的关键词
-     */
-    private final String searchKeywords;
 
     /**
      * 群聊数据层
@@ -36,14 +38,9 @@ public class LanZuoCloudResourceSearch implements Runnable {
     private DataGroup dataGroup;
 
 
-    public LanZuoCloudResourceSearch(String searchKeywords, DataGroup dataGroup) {
-        this.searchKeywords = searchKeywords;
-        this.dataGroup = dataGroup;
-    }
-
-
     @Override
     public void run() {
+        String key = InformationUtil.subEqual("=", dataGroup.getRaw_message());
         StringBuilder messageBuilder = new StringBuilder();
         int index = 0, eligibleIndex = 0;
         HashMap<String, String> sendMap = new HashMap<>();
@@ -52,7 +49,7 @@ public class LanZuoCloudResourceSearch implements Runnable {
         SignalUtil.sendGroupMessage(dataGroup.getGroup_id(), "正在搜索中,请稍等...");
         for (int i = 0; i < readJSONArray.size(); i++) {
             String name = readJSONArray.getByPath(String.format("[%s]name", i), String.class);
-            if (!(name.contains(searchKeywords))) {
+            if (!(name.contains(key))) {
                 continue;
             }
             //循环状态时符合条件的数量达到5个时执行分批发送并清空记录值和StringBuilder
@@ -78,7 +75,6 @@ public class LanZuoCloudResourceSearch implements Runnable {
         }
     }
 
-
     @SneakyThrows
     private void startGet(StringBuilder messageBuilder) {
         //休息一秒之后继续发送
@@ -92,4 +88,38 @@ public class LanZuoCloudResourceSearch implements Runnable {
         log.info("发送成功!");
     }
 
+    /**
+     * 权重,权重高的值会先匹配
+     *
+     * @return 权重值
+     */
+    @Override
+    public int weight() {
+        return 0;
+    }
+
+    /**
+     * 接受消息
+     *
+     * @param jsonObject 原始消息对象
+     * @param message    消息对象
+     * @return 是否匹配成功
+     */
+    @Override
+    public boolean onMessage(JSONObject jsonObject, Message message) {
+        if (!("group".equals(message.getMessage_type()))) {
+            return false;
+        }
+        this.dataGroup = BeanUtil.toBean(jsonObject, DataGroup.class);
+        String raw_message = dataGroup.getRaw_message();
+        if (!(raw_message.startsWith("蓝奏云资源搜索="))) {
+            return false;
+        }
+        String key = InformationUtil.subEqual("=", raw_message);
+        if (key.isEmpty()) {
+            return false;
+        }
+        this.run();
+        return false;
+    }
 }
