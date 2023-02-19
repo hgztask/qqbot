cea1bf3b658a77feefed9f02fa4074412fb0e399
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-19 17:15:29 +0800

    更新该类

--- src/main/java/com/example/qqbot/model/pgr/PGRModel.java
+++ src/main/java/com/example/qqbot/model/pgr/PGRModel.java
@@ -1,8 +1,11 @@
 package com.example.qqbot.model.pgr;
 
 import cn.hutool.core.collection.ListUtil;
+import cn.hutool.json.JSONArray;
+import com.example.qqbot.Util.InformationUtil;
 import com.example.qqbot.Util.SignalUtil;
 import com.example.qqbot.data.group.DataGroup;
+import com.example.qqbot.data.json.DataJson;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -50,14 +53,22 @@ public class PGRModel implements Runnable {
 
     /**
      * 打印战双意识表
+     *
      * @param group_id 发送的群号
-     * @param user_id 接受的QQ号,艾特对方
+     * @param user_id  接受的QQ号,艾特对方
+     *                 //https://s1.ax1x.com/2023/02/18/pSLr3xe.png
      */
-    public static void consciousnessPGRTable(String group_id,String user_id) {
+    public static void consciousnessPGRTable(String group_id, String user_id) {
         //针对于发图片的类型,目前测试需要带上file参数,这个应该是必带,试了不带发不出去,例外图片如果是网络链接的最好是图片格式的那种,以此为戒
-        if (SignalUtil.sendGroupMessage(group_id, String.format("[CQ:at,qq=%s]意识推荐共鸣配队站位表\n" +
-                "可用于角色意识搭配、意识共鸣、组队推荐\n" +
-                "看不清的请放大加载原图\n[CQ:image,file=战双意识表,url=https://s1.ax1x.com/2023/01/18/pS3Iekq.png]", user_id)).isEmpty()) {
+        String url="https://s1.ax1x.com/2023/02/18/pSLr3xe.png";
+        JSONArray nodeJson = new JSONArray();
+        nodeJson.add(DataJson.at(user_id));
+        nodeJson.add(DataJson.text("意识推荐共鸣配队站位表\n"+
+                "可用于角色意识搭配、意识共鸣、组队推荐\n"+
+                        "看不清的请放大加载原图\n"));
+        String imageFileName= InformationUtil.subEqual("/",url);
+        nodeJson.add(DataJson.imageUrl(imageFileName,url,true));
+        if (SignalUtil.sendGroupMessage(group_id,nodeJson).isEmpty()) {
             log.info("发送战双=意识表失败!");
             return;
         }
@@ -66,10 +77,11 @@ public class PGRModel implements Runnable {
 
     /**
      * 打印工会信息
+     *
      * @param group_id
      * @param user_id
      */
-    public static void printPGRGuild(String group_id,String user_id) {
+    public static void printPGRGuild(String group_id, String user_id) {
         SignalUtil.sendGroupMessage(group_id, String.format("[CQ:at,qq=%s]本群的公会\n" +
                 "公会名：桐子酱的光剑\n" +
                 "信标服公会ID：00008433\n" +
