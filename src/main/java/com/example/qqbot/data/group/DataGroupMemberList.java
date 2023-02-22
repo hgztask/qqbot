fc1207c67a1b7f8581a71907dbb45c41be186403
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-22 21:04:00 +0800

    群成员列表数据层

---
+++ src/main/java/com/example/qqbot/data/group/DataGroupMemberList.java
@@ -0,0 +1,98 @@
+package com.example.qqbot.data.group;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+/**
+ *
+ * 群成员列表数据层
+ * @author byhgz
+ * @version 1.0
+ * @date 2023/2/22 20:41
+ */
+@Data
+public class DataGroupMemberList {
+
+    /**
+     * 群号
+     */
+    private String group_id;
+    /**
+     * QQ号
+     */
+    private String user_id;
+    /**
+     * 昵称
+     */
+    private String nickname;
+    /**
+     * 群名片-备注
+     */
+    private String card;
+    /**
+     * 性别, male 或 female 或 unknown
+     */
+    private String sex;
+    /**
+     * 年龄
+     */
+    private int age;
+    /**
+     * 地区
+     */
+    private String area;
+    /**
+     * 加群时间戳
+     */
+    private int join_time;
+    /**
+     * 最后发言时间戳
+     */
+    private int last_sent_time;
+    /**
+     * 成员等级
+     */
+    private String level;
+    /**
+     * 角色, owner 或 admin 或 member
+     */
+    private String role;
+    /**
+     * 是否不良记录成员
+     */
+    private boolean unfriendly;
+    /**
+     * 专属头衔
+     */
+    private String title;
+    /**
+     * 专属头衔过期时间戳
+     */
+    private int title_expire_time;
+    /**
+     * 是否允许修改群名片
+     */
+    private boolean card_changeable;
+    /**
+     * 禁言到期时间
+     */
+    private int shut_up_timestamp;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DataGroupMemberList that = (DataGroupMemberList) o;
+        return Objects.equals(group_id, that.group_id) && Objects.equals(user_id, that.user_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(group_id, user_id);
+    }
+}
