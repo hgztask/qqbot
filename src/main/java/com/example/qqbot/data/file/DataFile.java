2f54ad7a92bcc8a1c6b352b27c4653464b3a20e6
hgztask <79256242+hgztask@users.noreply.github.com>
2023-02-26 18:36:23 +0800

    更新

--- src/main/java/com/example/qqbot/data/file/DataFile.java
+++ src/main/java/com/example/qqbot/data/file/DataFile.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
+import java.util.Set;
 
 /**
  * @author byhgz
@@ -17,7 +18,6 @@ import java.util.Random;
  * @date 2023/2/16 11:28
  */
 public class DataFile {
-
 
 
     private static final JSONObject ysImageJsonObj = JSONUtil.readJSONObject(new File("E:\\PCwinResouceDisk\\Download\\img\\原神系列.json"), StandardCharsets.UTF_8);
@@ -30,6 +30,10 @@ public class DataFile {
 
     private static final JSONObject HASH_MAP_COLLECTION = JSONUtil.readJSONObject(new File("E:\\PCwinResouceDisk\\Download\\img\\hashMap集合.json"), StandardCharsets.UTF_8);
 
+
+    @Getter
+    private static final List<String> HASH_MAPGATHER = new ArrayList<>(JSONUtil.readJSONObject(new File("E:\\PCwinResouceDisk\\Download\\img\\hashMap集合.json"), StandardCharsets.UTF_8).keySet());
+
     @Getter
     private static final List<String> LEG_SERIESL_IST = new ArrayList<>(LEG_SERIESOBJ.keySet());
 
@@ -39,11 +43,28 @@ public class DataFile {
     /**
      * 随机从集合里取出一个值
      *
-     * @return
+     * @param list 集合
+     * @return 值
      */
     public static String getRandomKey(List<String> list) {
         int i = InformationUtil.getRANDOM().nextInt(list.size());
         return list.get(i);
+    }
+
+
+    /**
+     * 随机从集合中取出一组值
+     *
+     * @param list 集合
+     * @param size 多少个(最大不会超过该数量)
+     * @return
+     */
+    public static Set<String> getRandomKeyList(List<String> list, int size) {
+        HashSet<String> set = new HashSet<>(size);
+        for (int i = 0; i < size; i++) {
+            set.add(getRandomKey(list));
+        }
+        return set;
     }
 
 
